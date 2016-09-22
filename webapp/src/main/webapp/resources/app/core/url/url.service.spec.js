c67567f2454fd8363059112948f95903c8e6bc3c
vvsvintsitsky <v.v.svintsitsky@gmail.com>
2016-09-22 15:24:27 +0300

    The rest of the web application tests.

---
+++ webapp/src/main/webapp/resources/app/core/url/url.service.spec.js
@@ -0,0 +1,282 @@
+describe("core.url", function() {
+
+	beforeEach(module("core.url"));
+
+	var $httpBackend;
+	var urlService;
+	var result;
+	var authHeaderName = 'Authentication';
+	var authHeaderValue = 'xxx';
+
+	beforeEach(angular.mock.module('ngCookies', 'core.url'));
+	beforeEach(inject(function(_$httpBackend_, _urlService_, $cookies) {
+		urlService = _urlService_;
+		$httpBackend = _$httpBackend_;
+		$cookies.remove(authHeaderName);
+		result = null;
+	}));
+	
+	afterEach(function() {
+	     $httpBackend.verifyNoOutstandingExpectation();
+	     $httpBackend.verifyNoOutstandingRequest();
+	   });
+
+	it('should contain an urlService', function() {
+		var serviceCondition = typeof urlService != 'undefined';
+		expect(serviceCondition).toBe(true);
+	});
+
+	it('should test getInfo function success', function() {
+		var responseTags = [ {
+			id : 0,
+			description : 'description0',
+			urls : null
+		}, {
+			id : 1,
+			description : 'description1',
+			urls : null
+		} ];
+		var short = 'shortUrl';
+		var responseUrl = {
+			id : 0,
+			shortUrl : short,
+			longUrl : 'longUrl',
+			visited : 0,
+			description : 'description',
+			accounts : null,
+			tags : responseTags
+		};
+		$httpBackend.expectGET(urlService.infoPath + short).respond(200,
+				responseUrl);
+		urlService.getInfo(short).then(function(returnFromPromise) {
+			result = returnFromPromise;
+		});
+		$httpBackend.flush();
+		expect(result).toEqual(responseUrl);
+	});
+
+	it('should test getInfo function failure', function() {
+
+		var short = 'shortUrl';
+		var errorInfo = {
+			url : urlService.infoPath + short,
+			ex : 'error'
+		};
+		$httpBackend.expectGET(urlService.infoPath + short).respond(404,
+				errorInfo);
+		urlService.getInfo(short).then(function(returnFromPromise) {
+			result = returnFromPromise;
+		});
+		$httpBackend.flush();
+		expect(result).toEqual(errorInfo);
+	});
+
+	it('should test getUsersUrls function success', inject(function($cookies) {
+
+		var responseUrls = [ {
+			id : 0,
+			shortUrl : 'shortUrl',
+			longUrl : 'longUrl',
+			visited : 0,
+			description : 'description',
+			accounts : null,
+			tags : null
+		} ];
+
+		expect($cookies.get(authHeaderName)).toBe(undefined);
+
+		$cookies.put(authHeaderName, authHeaderValue);
+		var headers = {
+			'Authentication' : $cookies.get(authHeaderName),
+			'Accept' : 'application/json, text/plain, */*'
+		};
+		$httpBackend.expectGET(urlService.servicePath, headers).respond(200,
+				responseUrls);
+		urlService.getUsersUrls().then(function(returnFromPromise) {
+			result = returnFromPromise;
+		});
+		$httpBackend.flush();
+		expect(result).toEqual(responseUrls);
+	}));
+
+	it('should test getUsersUrls function failure', inject(function($cookies) {
+
+		var errorInfo = {
+			url : urlService.servicePath,
+			ex : 'error'
+		};
+
+		expect($cookies.get(authHeaderName)).toBe(undefined);
+
+		$cookies.put(authHeaderName, authHeaderValue);
+		var headers = {
+			'Authentication' : $cookies.get(authHeaderName),
+			'Accept' : 'application/json, text/plain, */*'
+		};
+		$httpBackend.expectGET(urlService.servicePath, headers).respond(404,
+				errorInfo);
+		urlService.getUsersUrls().then(function(returnFromPromise) {
+			result = returnFromPromise;
+		});
+		$httpBackend.flush();
+		expect(result).toEqual(false);
+	}));
+	
+	it('should test updateUrl function success', inject(function($cookies) {
+
+		var requestTags = [ {
+			id : null,
+			description : 'description0',
+			urls : null
+		}, {
+			id : null,
+			description : 'description1',
+			urls : null
+		} ];
+		var requestUrl = {
+			id : null,
+			shortUrl : 'shortUrl',
+			longUrl : null,
+			visited : null,
+			description : null,
+			accounts : null,
+			tags : requestTags
+		};
+
+		expect($cookies.get(authHeaderName)).toBe(undefined);
+
+		$cookies.put(authHeaderName, authHeaderValue);
+		var headers = {
+			'Authentication' : $cookies.get(authHeaderName),
+			'Accept' : 'application/json, text/plain, */*',
+			'Content-Type' : 'application/json;charset=utf-8'
+		};
+		$httpBackend.expectPUT(urlService.servicePath, requestUrl, headers).respond(200,
+				'');
+		urlService.updateUrl(requestUrl).then(function(returnFromPromise) {
+			result = returnFromPromise;
+		});
+		
+		$httpBackend.flush();
+		expect(result).toEqual(true);
+	}));
+	
+	it('should test updateUrl function failure', inject(function($cookies) {
+
+		var requestTags = [ {
+			id : null,
+			description : 'description0',
+			urls : null
+		}, {
+			id : null,
+			description : 'description1',
+			urls : null
+		} ];
+		var requestUrl = {
+			id : null,
+			shortUrl : 'shortUrl',
+			longUrl : null,
+			visited : null,
+			description : null,
+			accounts : null,
+			tags : requestTags
+		};
+
+		expect($cookies.get(authHeaderName)).toBe(undefined);
+
+		$cookies.put(authHeaderName, authHeaderValue);
+		var headers = {
+			'Authentication' : $cookies.get(authHeaderName),
+			'Accept' : 'application/json, text/plain, */*',
+			'Content-Type' : 'application/json;charset=utf-8'
+		};
+		$httpBackend.expectPUT(urlService.servicePath, requestUrl, headers).respond(404,
+				'');
+		urlService.updateUrl(requestUrl).then(function(returnFromPromise) {
+			result = returnFromPromise;
+		});
+		
+		$httpBackend.flush();
+		expect(result).toEqual(false);
+	}));
+	
+	it('should test createUrl function success', inject(function($cookies) {
+
+		var requestTags = [ {
+			id : null,
+			description : 'description0',
+			urls : null
+		}, {
+			id : null,
+			description : 'description1',
+			urls : null
+		} ];
+		var description = 'description';
+		var longUrl = 'longUrl';
+		var requestUrl = {
+				id: null,
+				account: null,
+				shortUrl: null,
+				visited: null,
+				description: description,
+				longUrl: longUrl,
+				tags: requestTags
+		}
+		expect($cookies.get(authHeaderName)).toBe(undefined);
+
+		$cookies.put(authHeaderName, authHeaderValue);
+		var headers = {
+			'Authentication' : $cookies.get(authHeaderName),
+			'Accept' : 'application/json, text/plain, */*',
+			'Content-Type' : 'application/json;charset=utf-8'
+		};
+		$httpBackend.expectPOST(urlService.servicePath, requestUrl, headers).respond(200,
+				requestUrl);
+		urlService.createUrl(longUrl, description, requestTags).then(function(returnFromPromise) {
+			result = returnFromPromise;
+		});
+		
+		$httpBackend.flush();
+		expect(result).toEqual(requestUrl);
+	}));
+	
+	it('should test createUrl function failure', inject(function($cookies) {
+
+		var requestTags = [ {
+			id : null,
+			description : 'description0',
+			urls : null
+		}, {
+			id : null,
+			description : 'description1',
+			urls : null
+		} ];
+		var description = 'description';
+		var longUrl = 'longUrl';
+		var requestUrl = {
+				id: null,
+				account: null,
+				shortUrl: null,
+				visited: null,
+				description: description,
+				longUrl: longUrl,
+				tags: requestTags
+		}
+		expect($cookies.get(authHeaderName)).toBe(undefined);
+
+		$cookies.put(authHeaderName, authHeaderValue);
+		var headers = {
+			'Authentication' : $cookies.get(authHeaderName),
+			'Accept' : 'application/json, text/plain, */*',
+			'Content-Type' : 'application/json;charset=utf-8'
+		};
+		$httpBackend.expectPOST(urlService.servicePath, requestUrl, headers).respond(404,
+				'');
+		urlService.createUrl(longUrl, description, requestTags).then(function(returnFromPromise) {
+			result = returnFromPromise;
+		});
+		
+		$httpBackend.flush();
+		expect(result).toEqual('');
+	}));
+});
---
+++ webapp/src/test/java/wsvintsitsky/shortener/webapp/test/RegistrationControllerTest.java
@@ -0,0 +1,142 @@
+package wsvintsitsky.shortener.webapp.test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import wsvintsitsky.shortener.datamodel.Account;
+import wsvintsitsky.shortener.datamodel.Tag;
+import wsvintsitsky.shortener.datamodel.Url;
+import wsvintsitsky.shortener.service.AccountService;
+import wsvintsitsky.shortener.service.TagService;
+import wsvintsitsky.shortener.service.UrlService;
+import wsvintsitsky.shortener.webapp.datamodel.AccountWeb;
+import wsvintsitsky.shortener.webapp.resource.ConfigurationManager;
+import wsvintsitsky.shortener.webapp.security.WebTokenManager;
+import wsvintsitsky.shortener.webapp.test.database.filler.DatabaseFiller;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:web-applicationContext-test.xml" })
+@WebAppConfiguration
+public class RegistrationControllerTest {
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private AccountService accountService;
+
+	@Autowired
+	private UrlService urlService;
+
+	@Autowired
+	private TagService tagService;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setUp() {
+		wipeDB();
+		fillDatabase(2);
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	private void fillDatabase(int entityCount) {
+		int multiplier = 2;
+		DatabaseFiller filler = new DatabaseFiller();
+		List<Account> accounts = filler.createAccounts(entityCount);
+		List<Url> urls = filler.createUrls(accounts.size() * multiplier);
+		List<Tag> tags = filler.createTags(urls.size() * multiplier);
+		Account account;
+		Url url;
+		Tag tag;
+		int i;
+		int j;
+		int n;
+		int urlStart;
+		int tagStart;
+		for (i = 0; i < entityCount; i++) {
+			account = accounts.get(i);
+			accountService.saveOrUpdate(account);
+			urlStart = i * multiplier;
+			for (j = urlStart; j < urlStart + multiplier; j++) {
+				url = urls.get(j);
+				url.setAccount(account);
+				urlService.saveOrUpdate(url);
+				tagStart = j * multiplier;
+				for (n = tagStart; n < tagStart + multiplier; n++) {
+					tag = tags.get(n);
+					tag.getUrls().add(url);
+					tagService.saveOrUpdate(tag);
+				}
+			}
+		}
+	}
+
+	private void wipeDB() {
+		tagService.deleteAll();
+		urlService.deleteAll();
+		accountService.deleteAll();
+	}
+
+	@Test
+	public void testRegister() throws Exception {
+		AccountWeb accountWeb = new AccountWeb();
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonObject = mapper.writeValueAsString(accountWeb);
+
+		mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(jsonObject))
+				.andExpect(status().isBadRequest());
+
+		accountWeb.setEmail("webEmail@a.com");
+		jsonObject = mapper.writeValueAsString(accountWeb);
+		mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(jsonObject))
+				.andExpect(status().isBadRequest());
+
+		accountWeb.setEmail(null);
+		accountWeb.setPassword("webPassword");
+		jsonObject = mapper.writeValueAsString(accountWeb);
+		mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(jsonObject))
+				.andExpect(status().isBadRequest());
+
+		accountWeb.setPassword(null);
+		accountWeb.setEmail("webEmail");
+		jsonObject = mapper.writeValueAsString(accountWeb);
+		mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(jsonObject))
+				.andExpect(status().isBadRequest());
+
+		accountWeb.setEmail("webEmail@a.com");
+		accountWeb.setPassword("webPassword");
+		jsonObject = mapper.writeValueAsString(accountWeb);
+		mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(jsonObject))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testConfirm() throws Exception {
+		Account account = accountService.getAll().get(0);
+		String jwt = WebTokenManager.createJWT(account.getEmail(), account.getPassword());
+		String strings[] = jwt.split("\\.");
+		mockMvc.perform(post("/register/confirm")
+				.param(ConfigurationManager.getProperty("jwt.confirmation.first"), strings[0])
+				.param(ConfigurationManager.getProperty("jwt.confirmation.second"), strings[1])
+				.param(ConfigurationManager.getProperty("jwt.confirmation.third"), strings[2]))
+				.andExpect(status().isOk()).andExpect(content().string("true"));
+	}
+}
